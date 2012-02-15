dfd235d6a6e2fb02b932ac7617d6df8aafb484dd
Rafael Santos <raffaell.js@gmail.com>
2012-02-15 00:48:59 -0200

    adicionado tabela e campos de texto

--- agenda_gui.c
+++ agenda_gui.c
@@ -10,9 +10,12 @@ void sair(GtkWidget *w, gpointer *p){
 int main(int argc, char **argv){
 	
 
-	GtkWidget *window, 
+	GtkWidget	*window, 
 						*table, 
-						*hbox, 
+						*hbox_add_remove,
+						*hbox_ok_cancel,
+						*t_nome,
+						*t_telefone,
 						*b_add, 
 						*b_remove, 
 						*b_ok, 
@@ -32,24 +35,49 @@ int main(int argc, char **argv){
 	b_ok = gtk_button_new_with_label("Confirmar");
 	b_cancel = gtk_button_new_with_label("Cancelar");
 
+	t_nome = gtk_entry_new();
+	t_telefone = gtk_entry_new();
 
 	/**cria uma caixa horizontal.
 		 true = determina que todas as subdivisões tem o mesmo tamanho
 		 5 = qtd de pixels entre os componentes adicionados a caixa.
 	*/
-	hbox = gtk_hbox_new(TRUE,5);
+	hbox_add_remove = gtk_hbox_new(TRUE,5);
+	hbox_ok_cancel = gtk_hbox_new(TRUE,5);
+
+	//criando uma tabela de 4 linhas, 3 colunas e as celulas não terao o mesmo tamanho(FALSE).
+	table = gtk_table_new(4,3,TRUE);
+
+	
+	gtk_table_attach(GTK_TABLE(table),t_nome,1,3,0,1,(GTK_FILL | GTK_SHRINK),GTK_SHRINK,0,0);
+	gtk_table_attach(GTK_TABLE(table),t_telefone,1,3,1,2,(GTK_FILL | GTK_SHRINK),GTK_SHRINK,0,0);
 
 	//adicionar botao na caixa.	
-	gtk_box_pack_start(GTK_BOX(hbox),b_add,FALSE,TRUE,5);
-	gtk_box_pack_start(GTK_BOX(hbox),b_remove,TRUE,FALSE,5);
+	gtk_box_pack_start(GTK_BOX(hbox_add_remove),b_add,FALSE,TRUE,5);
+	gtk_box_pack_start(GTK_BOX(hbox_add_remove),b_remove,FALSE,TRUE,5);
 
-	//adiciona a caixa hbox em window
-	gtk_container_add(GTK_CONTAINER(window),hbox);
+	gtk_box_pack_start(GTK_BOX(hbox_ok_cancel),b_ok,FALSE,TRUE,5);
+	gtk_box_pack_start(GTK_BOX(hbox_ok_cancel),b_cancel,FALSE,TRUE,5);
 	
+	//adiciona as caixas na table
+	gtk_table_attach(GTK_TABLE(table),hbox_add_remove,1,2,2,3,GTK_FILL,GTK_SHRINK,0,0);	
+	gtk_table_attach(GTK_TABLE(table),hbox_ok_cancel,1,2,3,4,GTK_FILL,GTK_SHRINK,0,0);
+
+	//adiciona a table em window
+	gtk_container_add(GTK_CONTAINER(window),table);
+
+	
+
 	//Exibe os widgets
+	gtk_widget_show(t_nome);
+	gtk_widget_show(t_telefone);
 	gtk_widget_show(b_add);
 	gtk_widget_show(b_remove);
-	gtk_widget_show(hbox);
+	gtk_widget_show(hbox_add_remove);
+	gtk_widget_show(b_ok);
+	gtk_widget_show(b_cancel);
+	gtk_widget_show(hbox_ok_cancel);
+	gtk_widget_show(table);
 	gtk_widget_show(window);
 
 	gtk_main();
