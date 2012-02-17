f52c8abd88b7b69711eed4999a55d3db0232dfd7
Rafael Santos <raffaell.js@gmail.com>
2012-02-16 23:23:15 -0200

    Organização do Código

--- agenda_gui.c
+++ agenda_gui.c
@@ -1,5 +1,5 @@
 #include <gtk/gtk.h>
-#include <stdio.h>
+#include <stdlib.h> //para uso da função malloc.
 
 void sair(GtkWidget *w, gpointer *p){
 	g_print("Saindo...");
@@ -13,23 +13,15 @@ struct teste{
 };
 
 void adiciona(GtkWidget *w, gpointer *p){
-
 	struct teste *novoContato;
-
-	GtkWidget *entry;
-
 	novoContato = (struct teste*) malloc(sizeof(struct teste*));
-
 	novoContato = (struct teste*) p;
 
 	gchar *dados[2];
-
 	dados[0] = (char *) gtk_entry_get_text( GTK_ENTRY(novoContato->nome));
 	dados[1] = (char *) gtk_entry_get_text( GTK_ENTRY(novoContato->fone));
 
 	gtk_clist_append(GTK_CLIST(novoContato->lista),dados);
-
-	
 }
 
 int main(int argc, char **argv){
@@ -50,82 +42,54 @@ int main(int argc, char **argv){
 						*clist;
 
 	
-	//titulos da CList
 	gchar *titulos[2]={"Nome","Telefone"};
-	
-
 	struct teste *novoContato;
-
 	novoContato = (struct teste*) malloc(sizeof(struct teste*));
 
 
 	gtk_init(&argc,&argv);
 
-	//criando a janela e definindo tamanho, titulo e evendo ao fechar.
 	window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
 	gtk_widget_set_usize(GTK_WIDGET(window),500,500);
 	gtk_window_set_title(GTK_WINDOW(window),"Agenda Gtk");
 	gtk_signal_connect(GTK_OBJECT(window),"destroy",GTK_SIGNAL_FUNC(sair),NULL);
-	
-	//criando os botões
 	b_add = gtk_button_new_with_label("Adiciona");
 	b_remove = gtk_button_new_with_label("Remove");
 	b_ok = gtk_button_new_with_label("Confirmar");
-	b_cancel = gtk_button_new_with_label("Cancelar");
-
-
-	
+	b_cancel = gtk_button_new_with_label("Sair");
 	l_nome = gtk_label_new("Nome:");
 	l_telefone = gtk_label_new("telefone:");
-	
 	t_nome = gtk_entry_new();
 	t_telefone = gtk_entry_new();
-
 	clist = gtk_clist_new_with_titles(2,titulos);
 	gtk_clist_set_column_width(GTK_CLIST(clist),0,250);
 	gtk_clist_set_column_width(GTK_CLIST(clist),1,250);
+	hbox_add_remove = gtk_hbox_new(TRUE,5);
+	hbox_ok_cancel = gtk_hbox_new(TRUE,5);
+	table = gtk_table_new(5,3,FALSE);
+
 
 	novoContato->lista = clist;
 	novoContato->nome = t_nome;
 	novoContato->fone = t_telefone;
 
+	gtk_signal_connect(GTK_OBJECT(b_cancel),"clicked",GTK_SIGNAL_FUNC(sair),NULL);
 	gtk_signal_connect(GTK_OBJECT(b_add),"clicked",GTK_SIGNAL_FUNC(adiciona),novoContato);
 
-	/**cria uma caixa horizontal.
-		 true = determina que todas as subdivisões tem o mesmo tamanho
-		 5 = qtd de pixels entre os componentes adicionados a caixa.
-	*/
-	hbox_add_remove = gtk_hbox_new(TRUE,5);
-	hbox_ok_cancel = gtk_hbox_new(TRUE,5);
-
-	//criando uma tabela de 4 linhas, 3 colunas e as celulas não terao o mesmo tamanho(FALSE).
-	table = gtk_table_new(5,3,FALSE);
-
-
-	//alinhando o label a esquerda. obs: só que não alinha porra nenhum.
-	gtk_label_set_justify(GTK_LABEL(l_nome),GTK_JUSTIFY_LEFT);
-
+	gtk_box_pack_start(GTK_BOX(hbox_add_remove),b_add,FALSE,TRUE,5);
+	gtk_box_pack_start(GTK_BOX(hbox_add_remove),b_remove,FALSE,TRUE,5);
+	gtk_box_pack_start(GTK_BOX(hbox_ok_cancel),b_ok,FALSE,TRUE,5);
+	gtk_box_pack_start(GTK_BOX(hbox_ok_cancel),b_cancel,FALSE,TRUE,5);
+	
 	gtk_table_attach(GTK_TABLE(table),clist,0,3,0,2,(GTK_FILL | GTK_EXPAND),(GTK_FILL|GTK_EXPAND),0,0);
 	gtk_table_attach(GTK_TABLE(table),l_nome,0,1,2,3,GTK_FILL,GTK_SHRINK,0,0);
 	gtk_table_attach(GTK_TABLE(table),l_telefone,0,1,3,4,GTK_SHRINK,GTK_SHRINK,0,0);
 	gtk_table_attach(GTK_TABLE(table),t_nome,1,3,2,3,(GTK_FILL | GTK_SHRINK),GTK_SHRINK,0,0);
 	gtk_table_attach(GTK_TABLE(table),t_telefone,1,3,3,4,(GTK_FILL | GTK_SHRINK),GTK_SHRINK,0,0);
-
-	//adicionar botao na caixa.	
-	gtk_box_pack_start(GTK_BOX(hbox_add_remove),b_add,FALSE,TRUE,5);
-	gtk_box_pack_start(GTK_BOX(hbox_add_remove),b_remove,FALSE,TRUE,5);
-
-	gtk_box_pack_start(GTK_BOX(hbox_ok_cancel),b_ok,FALSE,TRUE,5);
-	gtk_box_pack_start(GTK_BOX(hbox_ok_cancel),b_cancel,FALSE,TRUE,5);
-	
-	//adiciona as caixas na table
 	gtk_table_attach(GTK_TABLE(table),hbox_add_remove,0,1,4,5,GTK_FILL,GTK_SHRINK,0,0);	
 	gtk_table_attach(GTK_TABLE(table),hbox_ok_cancel,2,3,4,5,GTK_FILL,GTK_SHRINK,0,0);
 
-	//adiciona a table em window
 	gtk_container_add(GTK_CONTAINER(window),table);
-
-	
 
 	//Exibe os widgets
 	gtk_widget_show(clist);
